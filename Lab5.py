1b7409ddc068def0241cea2e0a7538583c271948
SaraBarki <6290648@edu.vaniercollege.qc.ca>
2025-10-01 08:51:43 -0400

    FINALLY

--- Lab5.py
+++ Lab5.py
@@ -1,22 +1,62 @@
+import math
 # Example for n = 5:
 # *****
 # *   *
 # *   *
 # *   *
 # *****
+
 def hollow_square(n):
-    return ""
+    result = ""
+
+    for i in range(n):
+        if i == 0 or i == n-1:
+            for j in range(n):
+                result += '*'
+            result += '\n'
+        else:
+            # for k in range(n-2):
+            result += '*' + ' ' * (n - 2) + '*'
+            result += '\n'
+    
+
+    return result.rstrip()
+        
+
+
+print (hollow_square(5))
+
+
+
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    result = ''
+    for i in range(n):
+        for j in range(i + 1):
+            result += str(j + 1)
+        result += '\n'
+    result = result.rstrip()
+    return result
+    
+    
+
+print(number_pattern(4))
+
+
+
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    list = range(n + 1)
+    result = math.fsum(list)
+    return int(result)
+
+print(sum_of_natural_numbers(5))
+
 
 # Example for n = 4:
 #    *
@@ -24,4 +64,13 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    result = ''
+    for i in range(n):  
+         result +=  (n - 1 - i) * ' ' + '*' * (2 * i + 1) 
+         result += '\n'
+    return result.rstrip()
+print (centered_star_pyramid(4))
+
+
+# ' ' * ( (2 * n - 1) - (2 * i + 1) )  + '*' * (2 * i + 1) + ' ' *  ( (2 * n - 1) - (2 * i + 1) ) 
+# + ' ' * (n - 1 - i) maybe use this if not work
